54d2b97c3d5709bf6b5960af0aeea7e8d6c82b73
Marc Goujon <marc@rightscale.com>
2016-06-17 22:27:43 -0700

    maded wrapper multiplatform

--- wrapper.sh
+++ wrapper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 date=`date +%Y-%m-%d`;
-vim /Users/markitoxs/Code/markitoxs.github.com/_posts/$date-$1.markdown;
-cd /Users/markitoxs/Code/markitoxs.github.com/ ;
+vim ~/Code/markitoxs.github.com/_posts/$date-$1.markdown;
+cd ~/Code/markitoxs.github.com/ ;
 git add _posts/$date-$1.markdown;
 git commit -a -m "New post";
 git push origin;
